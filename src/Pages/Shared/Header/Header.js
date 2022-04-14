48ee4f446803cd5cf830311eab9294de6ab25500
arifvtti <arif.vtti@gmail.com>
2022-04-14 17:53:35 +0600

    done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -15,21 +15,23 @@ const Header = () => {
                 <Navbar.Brand as={Link} to="/">
                     <img height={30} src={logo} alt="" />
                 </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="home#services">Services</Nav.Link>
                         <Nav.Link href="home#experts">Experts</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
+
                     <Nav>
                         <Nav.Link as={Link} to="about">About</Nav.Link>
+                        {
+                            (user || githubUser || fbUser) && <p className='text-white mt-3'>{user?.displayName || githubUser?.displayName || fbUser?.displayName}</p>
+                        }
+
+                        {
+                            fbUser && <img style={{ borderRadius: "50%", width: "30px", height: "30px" }} className='mx-2' src={fbUser.photoURL} alt="" />
+                        }
                         {
                             githubUser || user ?
                                 <button onClick={() => signOut(auth)} className='btn btn-outline text-white'>Sign Out</button>
@@ -39,13 +41,7 @@ const Header = () => {
                                 </Nav.Link>
                         }
                     </Nav>
-                    {
-                        (user || githubUser || fbUser) && <p className='text-white mt-3'>{user?.displayName || githubUser?.displayName || fbUser?.displayName}</p>
-                    }
 
-                    {
-                        fbUser && <img style={{ borderRadius: "50%", width: "30px", height: "30px" }} className='mx-2' src={fbUser.photoURL} alt="" />
-                    }
                 </Navbar.Collapse>
             </Container>
         </Navbar>
